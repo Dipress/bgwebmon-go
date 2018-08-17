81125736c8e6dcceac2b15c9358553b335958484
Dipress <budnikovdo@gmail.com>
2018-08-17 21:24:08 +0300

    try to test authenticate method

--- internal/server/auth/auth_test.go
+++ internal/server/auth/auth_test.go
@@ -16,6 +16,74 @@ const (
 	}`
 )
 
+type decoderMock struct{}
+
+func (d decoderMock) Decode(r *http.Request, model *Model) error {
+	return nil
+}
+
+type validatorMock struct{}
+
+func (v validatorMock) Validate(model Model) error {
+	return nil
+}
+
+type finderMock struct{}
+
+func (f finderMock) Find(login string, model *Model) error {
+	return nil
+}
+
+type checkerMock struct{}
+
+func (c checkerMock) Check(requestPassword, modelPassword string) error {
+	return nil
+}
+
+type tokenerMock struct{}
+
+func (t tokenerMock) Token(userID int, secretKey string) (string, error) {
+	return "", nil
+}
+
+func Test_authenticator_Authenticate(t *testing.T) {
+	tests := []struct {
+		name     string
+		request  *http.Request
+		response *Response
+		wantErr  bool
+	}{
+		{
+			name:     "status Ok",
+			request:  &http.Request{},
+			response: &Response{},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			auth := authenticator{
+				secretKey: "secret",
+				decoder:   decoderMock{},
+				validator: validatorMock{},
+				finder:    finderMock{},
+				checker:   checkerMock{},
+				tokener:   tokenerMock{},
+			}
+
+			err := auth.Authenticate(tt.request, tt.response)
+
+			if tt.wantErr {
+				assert.NoError(t, err)
+			}
+
+		})
+	}
+}
+
 func Test_jwtToken_Token(t *testing.T) {
 	tests := []struct {
 		name      string
